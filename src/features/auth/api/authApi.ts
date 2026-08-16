01c0f1e9172d4f51f4c7fff48c5bd031e4d79cdc
Luciano Giacomelli <lucianogiacomelli8@gmail.com>
2026-08-16 20:55:56 -0300

    Create authApi.ts

---
+++ src/features/auth/api/authApi.ts
@@ -0,0 +1,46 @@
+import { axiosClient } from "@/api/axiosClient";
+import {
+  AuthLoginRequestDto,
+  AuthLoginResponseDto,
+  RefreshTokenRequestDto,
+  ForgotPasswordRequestDto,
+  ResetPasswordRequestDto,
+} from "../types/auth.dto";
+
+export const authApi = {
+  login: async (
+    credentials: AuthLoginRequestDto,
+  ): Promise<AuthLoginResponseDto> => {
+    const { data } = await axiosClient.post<AuthLoginResponseDto>(
+      "/api/auth/login",
+      credentials,
+    );
+    return data;
+  },
+
+  logout: async (payload: RefreshTokenRequestDto): Promise<string> => {
+    const { data } = await axiosClient.post<string>(
+      "/api/auth/logout",
+      payload,
+    );
+    return data;
+  },
+
+  forgotPassword: async (
+    payload: ForgotPasswordRequestDto,
+  ): Promise<string> => {
+    const { data } = await axiosClient.post<string>(
+      "/api/auth/forgot-password",
+      payload,
+    );
+    return data;
+  },
+
+  resetPassword: async (payload: ResetPasswordRequestDto): Promise<string> => {
+    const { data } = await axiosClient.post<string>(
+      "/api/auth/reset-password",
+      payload,
+    );
+    return data;
+  },
+};
