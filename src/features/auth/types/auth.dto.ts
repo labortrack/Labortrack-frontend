27a5897225d9c3a0be278f5a7c068a26fb805fcb
Luciano Giacomelli <lucianogiacomelli8@gmail.com>
2026-08-16 20:18:18 -0300

    Create auth.dto.ts

---
+++ src/features/auth/types/auth.dto.ts
@@ -0,0 +1,31 @@
+// Requests
+export interface AuthLoginRequestDto {
+  email: string;
+  password: string;
+}
+
+export interface GoogleLoginRequestDto {
+  token: string;
+}
+
+export interface RefreshTokenRequestDto {
+  refreshToken: string;
+}
+
+export interface ForgotPasswordRequestDto {
+  email: string;
+}
+
+export interface ResetPasswordRequestDto {
+  token: string;
+  newPassword: string;
+}
+
+// Responses
+export interface AuthLoginResponseDto {
+  email: string;
+  message: string;
+  jwt: string;
+  refreshToken: string;
+  status: boolean;
+}
